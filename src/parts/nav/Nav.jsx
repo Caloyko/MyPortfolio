6e09a02b66fd6708fa480c5e61a0f65b3471d745
Pauline LUBET <pauline.lubet@epita.fr>
2022-12-26 19:55:52 +0100

    nav

--- src/parts/nav/Nav.jsx
+++ src/parts/nav/Nav.jsx
@@ -1,8 +1,17 @@
 import "./nav.css"
+import { useState } from 'react'
+import { GoHome } from "react-icons/go"
+import {FaRegUser} from "react-icons/fa"
+import {GrContact} from "react-icons/gr"
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#")
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><GoHome /></a>
+      <a href='#about' onClick={() => setActiveNav('#about')}  className={activeNav === '#about' ? 'active' : ''}><FaRegUser /></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')}  className={activeNav === '#contact' ? 'active' : ''}><GrContact /></a>
+    </nav>
   )
 }
 
