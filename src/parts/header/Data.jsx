1551978ce4ddb4370c671fc92f4b7467baa6535a
Pauline LUBET <pauline.lubet@epita.fr>
2022-12-28 12:16:20 +0100

    header + downolad Cv

--- src/parts/header/Data.jsx
+++ src/parts/header/Data.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import CV from "../../assets/CV.pdf"
 
 const Data = () => {
   return (
       <div className='header__data'>
           <h1 className='header__title'>I am <span>Pauline Lubet</span></h1>
-          <h4 className='header__subtitle'>FullStack Developer</h4>
+          <h5 className='header__subtitle'>FullStack Developer</h5>
           <p className='header__description'>Actually Student in 4th year at Epita. Bla Bla Bla</p>
-          <a href='#contact' className='btn lg'>Contact me</a>
+          <div className="header__btn">
+            <a href='#contact' className='btn lg'>Contact me</a>
+            <a href={CV} download className='btn'>Download CV</a>
+          </div>    
         
     </div>
   )
--- src/parts/header/Header.jsx
+++ src/parts/header/Header.jsx
@@ -7,22 +7,19 @@ import Data from "./Data"
 
 const Header = () => {
   return (
-    <header className="header section" id="header">
-      <div className="header__container container grid">
-        <div className="header__content grid">
-          <Socials />
-            <div className="main__header-image"></div>
-          <Data/>
-          
-          </div>
-        <div className="main__header-right">
-          <div className="main__header-font"></div>
-            <div className="main__header-image">
-              <img src={IMGME} alt="header image"/>
-            </div>
-          </div>
+    <section>
+      <div className="header__section">
+        <div className="header-left">
+        <Data/>
+        </div>
+              <div className="header-right">
+                  <div className="header-blur"> </div>
+                  <div className="header-image">
+                      <img src={IMGME} alt="image header"/>
+                  </div>
+              </div>
       </div>
-    </header>
+    </section>
   )
 }
 
